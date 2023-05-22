535b8edb1ca8cd4a1132415086a428ffa00a5883
Daniel Chiquete <115588244+danielcn18@users.noreply.github.com>
2023-05-22 16:58:50 -0700

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,81 @@
+// grab all elements
+const form = document.querySelector('[data-form]');
+const input = document.querySelector('[data-input]');
+const lists = document.querySelector('[data-lists]');
+console.log(input);
+
+// local storage
+class Storage {
+    static addToStorage(todoArr){
+        let storage = localStorage.setItem("todo", JSON.stringify(todoArr));
+        return storage;
+    }
+    static getStorage(){
+        let storage = localStorage.getItem("todo") === null ? [] : JSON.parse(localStorage.getItem("todo"));
+        return storage;
+    }
+}
+
+// empty array
+let todoArr = Storage.getStorage();
+
+// form part
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let id = Math.random() * 1000000;
+    const todo = new Todo(id, input.value);
+    todoArr = [...todoArr, todo];
+    UI.displayData();
+    UI.clearInput();
+    // add to storage
+    Storage.addToStorage(todoArr);
+});
+
+// make object instance 
+class Todo{
+    constructor(id, todo){
+        this.id = id;
+        this.todo = todo;
+    }
+}
+
+// display the todo in the DOM
+class UI {
+    static displayData(){
+        let displayData = todoArr.map((item) => {
+            return `
+                <div class="todo">
+                <p>${item.todo}</p>
+                <span class="remove" data-id=${item.id}>🗑️</span>
+                </div>`
+        });
+        lists.innerHTML = (displayData).join(" ");
+    }
+    static clearInput(){
+        input.value = "";
+    }
+    static removeTodo(){
+        lists.addEventListener('click', (e) => {
+            if(e.target.classList.contains("remove")){
+                e.target.parentElement.remove();
+            }
+            let btnId = e.target.dataset.id;
+            //remove from array
+            UI.removeArrayTodo(btnId);
+        });
+    }
+    static removeArrayTodo(id){
+        // note: "+id" just confirms "id" is a number;
+        // note: "!==" means 'is not equal to';
+        // note: 
+        todoArr = todoArr.filter((item) => item.id !== +id);
+        Storage.addToStorage(todoArr);
+    }
+}
+
+// once the browser is loaded
+window.addEventListener("DOMContentLoaded", () => {
+    UI.displayData();
+    // remove from the dom
+    UI.removeTodo();
+});
